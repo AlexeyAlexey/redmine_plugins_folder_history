4e0c0fffdb364182cae2b070cc939e3fe2f609e6
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2017-04-14 16:58:56 +0300

    filter  show page  were edded

--- app/controllers/redmine_plugins_folder_histories_controller.rb
+++ app/controllers/redmine_plugins_folder_histories_controller.rb
@@ -1,15 +1,37 @@
 class RedminePluginsFolderHistoriesController < ApplicationController
   unloadable
   layout 'admin'
+
+  helper :sort
+  include SortHelper
+
+
   before_filter :require_admin
 
 
   def index
 
-  	@changes = RedminePluginsFolderHistory.where(nil).last
+  	 
+
+  	sort_init 'created_at', 'asc'
+    sort_update %w(created_at)
+
+    @limit = per_page_option
+  	@changes_scope = RedminePluginsFolderHistory.where(nil)
+
+    if !params[:created_at_from].blank? and !params[:created_at_to].blank?
+      @changes_scope = @changes_scope.where("DATE(created_at) BETWEEN ? AND ?", params[:created_at_from], params[:created_at_to])
+    end
+
+  	@changes_pages = Paginator.new @changes_scope.count, @limit, params['page']
+
+  	@offset ||= @changes_pages.offset
+
+  	@changes = @changes_scope.order(sort_clause).limit(@limit).offset(@offset).to_a
 
   end
 
   def show
+  	@changes = RedminePluginsFolderHistory.where("id=?", params["id"]).to_a.first
   end
 end
